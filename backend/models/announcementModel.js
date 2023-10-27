6d2c5008d74b7dd0e685a62e05b0ec43c935a656
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-27 15:23:26 +0530

    create announcement model

---
+++ backend/models/announcementModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    file: {
+        type: String,
+        required: true,
+    },
+    sent_to: {
+        type: String,
+        required: true,
+    },
+    is_company_announcement: {
+        type: Boolean,
+        required: true,
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+    },
+    job_profile: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'jobProfile',
+    },
+});
+
+module.exports = mongoose.model("Announcement", announcementSchema);
