dc3a99f2c9b55b0ab4074468a803f67176e53ee9
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-27 15:23:51 +0530

    create jobprofile model

---
+++ backend/models/jobProfileModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const jobProfileSchema = new mongoose.Schema({
+    company_name: {
+        type: String,
+        required: [true, "Please enter a company id"],
+    },
+    offer_type: {
+        type: String,
+        required: [true, "Please enter a offer type"],
+    },
+    location: {
+        type: String,
+        required: [true, "Please enter a location"],
+    },
+    open_for: {
+        type: String,
+        required: [true, "Please enter a open for"],
+    },
+    cpi_criteria: {
+        type: String,
+        required: [true, "Please enter a cpi criteria"],
+    },
+    ctc: {
+        type: String,
+        required: [true, "Please enter a ctc"],
+    },
+    registration_start_date: {
+        type: Date,
+        required: [true, "Please enter a registration start date"],
+    },
+    registration_end_date: {
+        type: Date,
+        required: [true, "Please enter a registration end date"],
+    },
+    job_description: {
+        type: String,
+        required: [true, "Please enter a job description"],
+    },
+    job_description_file: {
+        type: String,
+        required: [true, "Please enter a job description file"],
+    },
+    applicants: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Student'
+        }
+    ],
+});
+
+module.exports = mongoose.model("jobProfile", jobProfileSchema);
