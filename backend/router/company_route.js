4d02941d2014f28dc3d1ed1b00cbf0cb3c4e0618
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-28 15:06:39 +0530

    add company routes

--- backend/router/company_route.js
+++ backend/router/company_route.js
@@ -1,6 +1,46 @@
-const express=require('express');
+const express = require('express');
+const router = express.Router();
+const protect = require('../middleware/authmiddleware');
+const {
+    authCompany,
+    registerCompany,
+    loginCompany,
+    getCompanyProfile,
+    logOutCompany,
+    addJobProfile,
+    viewJobProfile,
+    viewJobProfileById,
+    deleteJobProfileById,
+    updateJobProfileById,
+    applyJobProfileById,
+    viewApplicantsById,
+    announceResultById,
+    addAnnouncement,
+    viewAnnouncement,
+    viewAnnouncementById,
+    deleteAnnouncementById,
+    updateAnnouncementById,
+    viewStudentsByJobProfileId
+} = require('../controller/company_control');
 
-const router=express.Router();
+router.post('/auth', authCompany);
+router.post('/register', registerCompany);
+router.post('/login', loginCompany);
+router.get('/profile', protect, getCompanyProfile);
+router.post('/logout', logOutCompany);
+router.post('/addjobprofile', protect, addJobProfile);
+router.get('/viewjobprofile', protect, viewJobProfile);
+router.get('/viewjobprofile/:id', protect, viewJobProfileById);
+router.delete('/deletejobprofile/:id', protect, deleteJobProfileById);
+router.put('/updatejobprofile/:id', protect, updateJobProfileById);
+router.post('/applyjobprofile/:id', protect, applyJobProfileById);
+router.get('/viewapplicants/:id', protect, viewApplicantsById);
+router.put('/announceresult/:id', protect, announceResultById);
+router.post('/addannouncement', protect, addAnnouncement);
+router.get('/viewannouncement', protect, viewAnnouncement);
+router.get('/viewannouncement/:id', protect, viewAnnouncementById);
+router.delete('/deleteannouncement/:id', protect, deleteAnnouncementById);
+router.put('/updateannouncement/:id', protect, updateAnnouncementById);
+router.get('/viewstudentsbyjobprofileid/:id', protect, viewStudentsByJobProfileId);
 
-
-module.exports=router;
+module.exports = router;
