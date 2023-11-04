040ad521a00bb267a35da70eb9c8706e1be24761
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-04 15:47:17 +0530

    update validation for announcements

--- src/middleware/validatemiddleware.js
+++ src/middleware/validatemiddleware.js
@@ -30,8 +30,8 @@ const validateAnnouncement = [
     check('title').not().isEmpty().withMessage('Title is required'),
     check('description').not().isEmpty().withMessage('Description is required'),
     check('date').not().isEmpty().withMessage('Date is required'),
-    check('file').not().isEmpty().withMessage('File is required'),
-    check('sent_to').not().isEmpty().withMessage('Sent to is required'),
+    // check('file').not().isEmpty().withMessage('File is required'),
+    // check('sent_to').not().isEmpty().withMessage('Sent to is required'),
     check('is_company_announcement').not().isEmpty().withMessage('Is company announcement is required'),
     check('company').not().isEmpty().withMessage('Company is required'),
 
