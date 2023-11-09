d857bc67baa2143b055f4ac947dfe61ed39041d8
ShivangKacha <shivangkachasbk9@gmail.com>
2023-11-09 17:04:10 +0530

    profile pic upload

--- src/controller/fileController.js
+++ src/controller/fileController.js
@@ -2,12 +2,16 @@ const { uploadFile, retrieveFile } = require('../utils/fileservice');
 const student = require('../models/studentModel');
 
 const FileController = {
-    get: async (req, res) => {
+    get_resume: async (req, res) => {
         const { id } = req.params;
         await retrieveFile({ fileId: id, stream: res });
-
     },
-    post: async (req, res) => {
+    get_profilepic: async (req, res) => {
+        const { id } = req.params;
+        await retrieveFile({ fileId: id, stream: res });
+        res.set('content-typee', 'image / png', 'image / jpg')
+    },
+    resume: async (req, res) => {
         const { id } = await uploadFile({ file: req.file });
         // console.log(id);
         // sleep(5000);
@@ -25,7 +29,28 @@ const FileController = {
                 fileId: id,
             });
         }
-    }
+    },
+    profilepic: async (req, res) => {
+        const { id } = await uploadFile({ file: req.file });
+        // console.log(id);
+        // sleep(5000);
+        const stu = await student.findByIdAndUpdate(req.student._id, { profile_pic: id }, { new: true });
+        // console.log(stu);
+        if (stu) {
+            res.json({
+                message: 'File uploaded successfully',
+                fileId: id,
+
+            });
+        }
+        else {
+            res.json({
+                message: 'File not uploaded',
+                fileId: id,
+            });
+        }
+    },
+
 };
 
 module.exports = FileController;
