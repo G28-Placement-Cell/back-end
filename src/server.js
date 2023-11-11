7891d6f8b4da6639e0b1d4d6447d9a12c84acf08
ShivangKacha <shivangkachasbk9@gmail.com>
2023-11-12 00:41:55 +0530

    file limit changed

--- src/server.js
+++ src/server.js
@@ -22,8 +22,8 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '3mb' }));
+app.use(bodyParser.urlencoded({ limit: '3mb', extended: true }));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
