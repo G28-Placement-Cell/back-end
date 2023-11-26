f8344b746fd2e411e6af512043248595c8b04d3b
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-27 01:07:58 +0530

    protection added in announcements

--- src/router/announcementRoutes.js
+++ src/router/announcementRoutes.js
@@ -15,6 +15,8 @@ const {
     createAnnouncementByCompanyForStudent
 } = require('../controller/announcementController');
 const protect = require('../middleware/authmiddleware');
+const compprotect = require('../middleware/authCompMiddleware.js');
+const adminprotect = require('../middleware/authAdminMiddleware.js');
 const { validateAnnouncement } = require('../middleware/validatemiddleware.js');
 
 router.route('/').post(validateAnnouncement, createAnnouncement)
@@ -22,13 +24,17 @@ router.route('/').get(getAllAnnouncements);
 router.route('/:id').get(getAnnouncementById)
 router.route('/:id').put(validateAnnouncement, updateAnnouncement)
 router.route('/:id').delete(deleteAnnouncement);
-router.route('/admin/company').get(getAnnouncementsByAdminForCompany);
-router.route('/admin/student').get(getAnnouncementsByAdminForStudent)
-router.route('/admin/student').post(createAnnouncementByAdminForStudent);
-router.route('/admin/company').post(createAnnouncementByAdminForCompany);
-router.route('/admin/companyAnnouncements').get(getAnnouncementsByAllCompanies);
-router.route('/company/:id').get(getAnnouncementsByCompanyId);
-router.route('/company/:id').post(createAnnouncementByCompanyForStudent);
+router.route('/admin/company').get(adminprotect, getAnnouncementsByAdminForCompany);
+router.route('/admin/company/company').get(compprotect, getAnnouncementsByAdminForCompany);
+router.route('/admin/student').get(adminprotect, getAnnouncementsByAdminForStudent);
+router.route('/admin/student/student').get(protect, getAnnouncementsByAdminForStudent);
+router.route('/admin/student').post(adminprotect,createAnnouncementByAdminForStudent);
+router.route('/admin/company').post(compprotect,createAnnouncementByAdminForCompany);
+router.route('/admin/companyAnnouncements').get(adminprotect,getAnnouncementsByAllCompanies);
+router.route('/student/companyAnnouncements').get(protect,getAnnouncementsByAllCompanies);
+
+router.route('/company/:id').get(compprotect,getAnnouncementsByCompanyId);
+router.route('/company/:id').post(compprotect,createAnnouncementByCompanyForStudent);
 
 
 module.exports = router;
